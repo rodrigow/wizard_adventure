f0b98b3e079c170938ab2ab5959e23f9a2e87068
Rodrigo Guilherme Wolschick <rodrigogui@gmail.com>
2014-01-24 01:56:24 -0200

    describe paths

--- test/wizard-test.lisp
+++ test/wizard-test.lisp
@@ -5,3 +5,6 @@
 
 (define-test "should describe path to garden"
   (assert-equal '(THERE IS A DOOR GOING WEST FROM HERE.) (describe-path '(garden west door))))
+
+(define-test "should describe paths to living-room"
+  (assert-equal '(THERE IS A DOOR GOING WEST FROM HERE. THERE IS A LADDER GOING UPSTAIRS FROM HERE.) (describe-paths 'living-room *edges*)))
